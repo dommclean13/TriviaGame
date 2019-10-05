ac25c24ff4aa5292fd4c0b99ffb1c90c0b22cf32
Dominique McLean <dommclean13@gmail.com>
2019-10-05 18:05:48 -0500

    changes to all files

---
+++ assets/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $("#startClick").on("click", function () {
+        alert("good job")
+});
+
+// var score = 0;
+
+// var questions = [
+//     {
+//         question: "How did Harry Potter get his scar?",
+//         choices: ["He had an accident", "Vouldemort tried to kill him", "He was attacked by a bailik", "He crashed rons car"],
+//         answer: "Vouldemort tried to kill him",
+//     },
+//     {
+//         question: "Who is fluffy?" ,
+//         choices: ["Hermiones Cat", "Harrys owl", "Hagrids Dragon", "Three headed dog"],
+//         answer: "Three headed dog",
+//     },
+//     {
+//         question: "What house at Hogwarts does Harry belond to?",
+//         choices: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"],
+//         answer: "Gryffindor",
+//     },
+//     {
+//         question: "What does the Imperious curse do?",
+//         choices: ["Kills", "Controls", "Tourtures", "Turns the person into a pig"],
+//         answer: "Controls the actions of another person",
+//     },
+//     {
+//         question: "Who kills Proffessor Dumbledore",
+//         choices: ["Bellatrix", "Draco", "Servous", "Fenrir"],
+//         answer: "Severous Snape",
+//     },
+// ];
+
+
+
+// $("#startClick").on("click", function () {
+//     alert("good job")
+    // for (var i = 0; i < questions.length; i++) {
+    //     var response = $("#startBtn").append(questions);
+    //     console.log(questions[i]);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
